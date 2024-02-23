07a2d1961a098adb3ff2e612e59424630f5e0b35
Anton Knutsson <cykelkorg95@gmail.com>
2024-02-23 12:24:10 +0100

    carousel is now showing five random trailers

--- Template/scripts/script.js
+++ Template/scripts/script.js
@@ -4,16 +4,59 @@ import { setupCarousel } from "./carousel.js";
 import { fetchTrailerAPI } from "./APIhandler.js";
 import { myTopMovies } from "./APIhandler.js";
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
     console.log('load');
     //Förslagsvis anropar ni era funktioner som skall sätta lyssnare, rendera objekt osv. härifrån
-    setupCarousel();
-    fetchTrailerAPI();
+    // setupCarousel();
+    // fetchTrailerAPI();
     myTopMovies();
     showtopMovies();
+    try {
+        const allTrailers = await fetchTrailerAPI();
+
+        const randomTrailers = getRandomTrailers(allTrailers, 5);
+
+        carouselItems(randomTrailers);
+    } catch (error) {
+        console.error('Något gick fel vid hämtning av trailer', error);
+    }
 });
 
 
+// Funktion för att slumpa fem filmer från listan
+function getRandomTrailers(trailers, count) {
+    const shuffledTrailers = trailers.sort(() => Math.random() - 0.5);
+    return shuffledTrailers.slice(0, count);
+}
+
+// Skapa HTML element för varje trailer och lägg till dem i karusellen
+function carouselItems(trailers) {
+    const slidesContainer = document.querySelector('[data-slides]');
+
+    trailers.forEach((trailer, index) => {
+        const slide = document.createElement('div');
+        slide.classList.add('carousel__slide');
+        if(index === 0){
+            slide.dataset.active = true // Markera första sliden som aktiv
+        }
+
+        const trailerLink = document.createElement('iframe');
+        trailerLink.src = trailer.trailer_link;
+        trailerLink.width = '420';
+        trailerLink.height = '315';
+        trailerLink.frameborder = "0";
+
+        slide.appendChild(trailerLink);
+
+        // Lägg till data-carousel och data-slides attribut för att matcha setupCarousel-funktionen
+        slide.dataset.carousel = 'true';
+        slide.dataset.slides = 'true';
+
+        slidesContainer.appendChild(slide);
+    });
+
+    setupCarousel();
+}
 
 async function showtopMovies() {
 
