63f7580ffcc9b018ac9a56f87c49ac1e2a9fd70a
Anton Knutsson <cykelkorg95@gmail.com>
2024-02-23 17:39:36 +0100

    added searchMovieAPI function

--- Template/scripts/APIhandler.js
+++ Template/scripts/APIhandler.js
@@ -1,4 +1,4 @@
-
+/* Trailer carousel */
 async function fetchTrailerAPI(){
     try {
         const response = await fetch('https://santosnr6.github.io/Data/movies.json');
@@ -23,6 +23,8 @@ async function fetchTrailerAPI(){
 
 export { fetchTrailerAPI };
 
+/* Top 20 Movies */
+
 async function myTopMovies() {
     try {
         const response = await fetch('https://santosnr6.github.io/Data/movies.json');
@@ -46,8 +48,27 @@ async function myTopMovies() {
 
 export { myTopMovies };
 
+/* Movie search */
 
-async function renderMovies() {
-    
+async function searchMoviesAPI(apiKey, searchQuery) {
+    const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`;
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error('Kan inte hämta data från API..');
+        }
+
+        const searchData = await response.json();
+        console.log(searchQuery.value);
+        console.log('searchMoviesAPI()');
+        return searchData.Search;
+        
+    } catch (error) {
+        console.error('Ojsan, något gick fel. Kan inte hämta data från API', error);
+        return [];
+    }
 }
 
+export { searchMoviesAPI };
